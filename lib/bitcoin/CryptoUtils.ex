82f8141ecd8ababbc1233daf9d5d8864735b0acc
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-11-17 22:02:57 -0500

    Add utility functions for crypto.

---
+++ lib/bitcoin/CryptoUtils.ex
@@ -0,0 +1,23 @@
+defmodule CryptoUtils do
+  
+  def generate_keypair() do
+    {public_key, private_key} = :crypto.generate_key(:ecdh, :secp256k1)
+    %{
+      public_key: public_key,
+      private_key: private_key
+    }
+  end
+
+  def sign(keypair, message) do
+    :crypto.sign(:ecdsa, :sha256, message, [keypair.private_key, :secp256k1])
+  end
+
+  def verify(keypair, signature, message) do
+    :crypto.verify(:ecdsa, :sha256, message, signature, [keypair.public_key, :secp256k1])
+  end
+
+  def calculateHash(data) do
+    :crypto.hash(:sha, data)
+  end
+  
+end
