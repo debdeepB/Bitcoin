e724a25425fac622e086108256c7e1ab62cb98bc
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-11-18 21:42:47 -0500

    Add PendingTransactions genserver.

---
+++ lib/bitcoin/PendingTransactions.ex
@@ -0,0 +1,31 @@
+defmodule PendingTransactions do
+  use GenServer
+
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def add(pid, tx) do
+    GenServer.cast(pid, {:add, tx})
+  end
+
+  def get_pending_transactions(pid) do
+    GenServer.call(pid, :get_all)
+  end
+
+  @impl true
+  def init(queue) do
+    {:ok, queue}
+  end
+
+  @impl true
+  def handle_call(:get_all, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_cast({:add, tx}, state) do
+    {:noreply, state ++ [tx]}
+  end
+  
+end
